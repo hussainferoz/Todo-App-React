938868385a4db31872e51996619b84a19ed78440
Hussain <hussainferoz839@gmail.com>
2020-06-30 01:03:00 +0500

    Basic setup

---
+++ client/src/app/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles/main.css';
+
+//Routes
+import LoginRoute from './routes/LoginRoute';
+
+function App() {
+	return (
+		<div className="App">
+			<LoginRoute />
+		</div>
+	);
+}
+
+export default App;
