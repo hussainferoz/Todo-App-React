c675756aca6db538a0ef71e0305bcb622d127c73
Hussain <hussainferoz839@gmail.com>
2020-06-30 17:35:32 +0500

    todo page header content

--- client/src/app/components/Header.js
+++ client/src/app/components/Header.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import { Typography } from '@material-ui/core';
+import { AccountCircleOutlined } from '@material-ui/icons';
+
 const header = () => {
 	return (
 		<div className="header__container">
-			<div className="header__bar">Header Content will be added here</div>
+			<div className="header__bar">
+				<div className="header__bar__left">
+					<Typography variant="h6">The Todo App</Typography>
+				</div>
+				<div className="header__bar__right">
+					<AccountCircleOutlined />
+					<Typography>Hussain Feroz</Typography>
+				</div>
+			</div>
 		</div>
 	);
 };
