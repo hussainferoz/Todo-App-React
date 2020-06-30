ee3c1b28a4f3858205964a875c570d0fab336b84
Hussain <hussainferoz839@gmail.com>
2020-06-30 15:47:56 +0500

    login page form setup

--- client/src/app/containers/Login.js
+++ client/src/app/containers/Login.js
@@ -1,14 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { Typography, TextField, InputAdornment, IconButton, Button } from '@material-ui/core';
+
+import { VisibilityOutlined, VisibilityOffOutlined, VpnKeyOutlined, EmailOutlined } from '@material-ui/icons';
 
 const Login = () => {
+	const [ showPassword, setShowPassword ] = useState(false);
+	const [ password, setPassword ] = useState('');
+	const [ email, setEmail ] = useState('');
+
+	const handleClickShowPassword = () => {
+		setShowPassword(!showPassword);
+	};
+
+	const handleMouseDownPassword = (event) => {
+		event.preventDefault();
+	};
+
+	const handlePasswordChange = (event) => {
+		setPassword(event.target.value);
+	};
+
+	const handleEmailChange = (event) => {
+		setEmail(event.target.value);
+	};
+
 	return (
 		<div className="login__container">
 			<div className="login__content">
 				<div className="login__content__image">
-					<span>Welcome To The Todo App</span>
+					<Typography variant="h3">The Todo App</Typography>
 					<img src={require('../../assets/logo.png')} />
 				</div>
-				<div className="login__content__form" />
+				<div className="login__content__form">
+					<span className="form__heading">
+						<Typography variant="h4">Welcome,</Typography>
+						<Typography variant="h4">Login to continue</Typography>
+					</span>
+					<span className="form__field">
+						<TextField
+							className="textfield__input"
+							label="Enter Email"
+							value={email}
+							onChange={(event) => handleEmailChange(event)}
+							InputProps={{
+								startAdornment: (
+									<InputAdornment position="start">
+										<EmailOutlined />
+									</InputAdornment>
+								)
+							}}
+						/>
+						<TextField
+							className="textfield__input"
+							type={showPassword ? 'text' : 'password'}
+							label="Enter Password"
+							type={showPassword ? 'text' : 'password'}
+							value={password}
+							onChange={(event) => handlePasswordChange(event)}
+							InputProps={{
+								startAdornment: (
+									<InputAdornment position="start">
+										<VpnKeyOutlined />
+									</InputAdornment>
+								),
+								endAdornment: (
+									<InputAdornment position="end">
+										<IconButton
+											aria-label="toggle password visibility"
+											onClick={handleClickShowPassword}
+											onMouseDown={handleMouseDownPassword}
+										>
+											{showPassword ? <VisibilityOutlined /> : <VisibilityOffOutlined />}
+										</IconButton>
+									</InputAdornment>
+								)
+							}}
+						/>
+						<Button variant="contained" color="primary" className="textfield__input">
+							Login
+						</Button>
+					</span>
+				</div>
 			</div>
 		</div>
 	);
