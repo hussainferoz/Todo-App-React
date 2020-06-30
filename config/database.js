0d6448743847522f6626231f079c23a1efc965fa
Hussain <hussainferoz839@gmail.com>
2020-07-01 00:31:54 +0500

    mongodb connected

--- config/database.js
+++ config/database.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-	console.log(process.env.MONGOURI);
-	// try {
-	// 	await mongoose.connect(db, {
-	// 		useNewUrlParser: true,
-	// 		useCreateIndex: true,
-	// 		useFindAndModify: false
-	// 	});
+	try {
+		await mongoose.connect(process.env.MONGOURI, {
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+			useUnifiedTopology: true
+		});
 
-	// 	console.log('MongoDB Connected');
-	// } catch (error) {
-	// 	console.log(error.message);
-	// 	process.exit(1); //exit with failure
-	// }
+		console.log('MongoDB Connected');
+	} catch (error) {
+		console.log(error.message);
+		process.exit(1); //exit with failure
+	}
 };
 
 module.exports = connectDB;
