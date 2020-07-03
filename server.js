4ace8efe2caeb0c0b3934559332057c77482d1fc
Hussain <hussainferoz839@gmail.com>
2020-07-03 11:22:07 +0500

    production setup

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const path = require('path');
 
 const connectDb = require('./config/database');
 const authRouter = require('./routes/authentication');
 const todoRouter = require('./routes/todoRoute');
+const { response } = require('express');
 
 //Access env variables
 dotenv.config();
@@ -19,6 +21,12 @@ app.use(express.json({ extended: false }));
 //Routes
 app.use('/api/login', authRouter);
 app.use('/api/todo', todoRouter);
+
+//Load index.html from client/build
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+	app.get('*', (request, response) => response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const port = 5000;
 
